bab02ad18d7550bc2c6ca531fce108c534d8e6ad
Andres Barbudo Rodriguez <andresbarbudorodriguez@andress-macbook-pro.local>
2022-01-28 23:05:03 -0300

    Initial Commit

---
+++ Android Studio Kraken/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Android Studio Kraken
+//
+//  Created by Andres Barbudo Rodriguez on 1/28/22.
+//  Copyright © 2022 Andres Barbudo Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
