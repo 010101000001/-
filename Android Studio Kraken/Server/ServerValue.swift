3b45ae062fdba42b445626a7689be2cb888f0c01
AndrewStockton <98692524+AndrewStockton@users.noreply.github.com>
2022-02-16 12:12:22 -0300

    Constantes de Servidor
    errores de compatibilidad mas comunes

---
+++ Android Studio Kraken/Server/ServerValue.swift
@@ -0,0 +1,12 @@
+//
+//  ServerValue.swift
+//  
+//
+//  Created by Andres Barbudo Rodriguez on 2/16/22.
+//
+
+import Foundation
+import CloudKit/JS
+
+
+readonly attribute String serverErrorCode;
