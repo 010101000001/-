7b45524615936efc63f3c710d75d3d239c926731
AndrewStockton <98692524+AndrewStockton@users.noreply.github.com>
2022-02-16 12:11:51 -0300

    Servidor Android
    cabecera de red del servidor de google play services

---
+++ Android Studio Kraken/Server/ServerSocket.h
@@ -0,0 +1,13 @@
+//
+//  ServerSocket.h
+//  
+//
+//  Created by Andres Barbudo Rodriguez on 2/16/22.
+//
+
+#ifndef ServerSocket_h
+#define ServerSocket_h
+
+#include <stdio.h>
+
+#endif /* ServerSocket_h */
