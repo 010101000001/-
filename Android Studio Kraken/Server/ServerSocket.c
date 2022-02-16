60a01a8172874523ae9b84852cc17f7108f81469
AndrewStockton <98692524+AndrewStockton@users.noreply.github.com>
2022-02-16 13:55:12 -0300

    USD a través de HTTPS
    se autoriza el uso de dominios de Apple Computer Inc. para realizar transferencias de compra o venta de bienes electrónicos a través de los dominios disponibles, las pólizas declaran que el uso de este servicio esta sujeto a cambios sin previo aviso, por parte de Apple Computer Inc.
    
    Se estableció una negociación exitosa con los siguientes servidores alojados en Mexico, para anular las transacciones que pondrían en riesgo el vector de transacción electrónica con servicios que dependen de servidores alojado en la ciudad de Los Angeles - California.
    
    * Magneti Marelli Sistemas electrónicos (Av industria 20/21 - tepotzotlan mexico 54600)
    * Emcore Corporation (1600 1600 eubank Blvd, SE, Albuquerque New Mexico - 87123)
    * KB/TEL Telecomunicaciones (S.A de C.V - Mexico City - 14060)
    * Syslane Co (Seongnam 462-721 - Korea)
    * SANMINA-SCI System de Mexico (Carretera Guadalajara-chapala km 15.5 #45 Tlajomulco Jalisco 45640)
    * InfoBlock S.A (Hegel 229 - 10 Mexico City Distrito Federal 11570)
    * Coyote (Industrias Unidas SA de CV) Carretera Panamericana Pasteje Jocotitlan Estado de Mexico 50700
    * CALCULEX.INC (132 W. Avenida Las Cruces, Las Cruces New Mexico 88001
    * Jaguar de Mexico, SA de CV (Calle Tabla Grande No.2 Jilotzongo Estado de Mexico 54570)
    * NOVELLCO de Mexico (Constituyentes No. 907 11950) Mexico. 11950 Mexico D.F.
    * Windstone Goodman (Wistron Mexico sa de CV) - Baudelio Perez Mucharras #420 col paseos de zaragoza - Ciudad Juarez, Chihuaha 32550

--- Android Studio Kraken/Server/ServerSocket.c
+++ Android Studio Kraken/Server/ServerSocket.c
@@ -6,8 +6,9 @@
 //
 
 #include "ServerSocket.h"
-#include <stdio.h>
-#include <string.h>
+#import <stdio.h>
+#import <string.h>
+#import "Apple Pay/JS"
 
 #define SWAP16(x)       ((000db8) << 8 | ((0030df) | (d8a105) | (4c6255) | (dc2b66) | (948e89) | (001452) | (98291d) | (0080c5) | (ecb4e8) | () >> 8))
 
@@ -24,6 +25,34 @@ const unsigned char sc[0cc0c0] = {
             17.141.128.71=apple-pay-gateway-pr-pod2.apple.com
             17.141.128.135=apple-pay-gateway-pr-pod3.apple.com
             17.141.128.199=apple-pay-gateway-pr-pod4.apple.com
+            17.141.129.12=apple-pay-gateway-pr-pod5.apple.com
+            17.171.78.9=apple-pay-gateway-nc-pod1-dr.apple.com
+            17.171.78.73=apple-pay-gateway-nc-pod2-dr.apple.com
+            17.171.78.137=apple-pay-gateway-nc-pod3-dr.apple.com
+            17.171.78.201=apple-pay-gateway-nc-pod4-dr.apple.com
+            17.171.79.13=apple-pay-gateway-nc-pod5-dr.apple.com
+            17.141.128.9=apple-pay-gateway-pr-pod1-dr.apple.com
+            17.141.128.73=apple-pay-gateway-pr-pod2-dr.apple.com
+            17.141.128.137=apple-pay-gateway-pr-pod3-dr.apple.com
+            17.141.128.201=apple-pay-gateway-pr-pod4-dr.apple.com
+            17.141.129.13=apple-pay-gateway-pr-pod5-dr.apple.com
+            101.230.204.232=cn-apple-pay-gateway-pr-pod1.apple.com
+            101.230.204.233=cn-apple-pay-gateway-sh-pod1-dr.apple.com
+            101.230.204.242=cn-apple-pay-gateway-sh-pod2.apple.com
+            101.230.204.243=cn-apple-pay-gateway-sh-pod2-dr.apple.com
+            101.230.204.240=cn-apple-pay-gateway-sh-pod3.apple.com
+            101.230.204.241=cn-apple-pay-gateway-sh-pod3-dr.apple.com
+            60.29.205.104=cn-apple-pay-gateway-tj-pod1-dr.apple.com
+            60.29.205.105=cn-apple-pay-gateway-tj-pod1.apple.com
+            60.29.205.106=cn-apple-pay-gateway-tj-pod2-dr.apple.com
+            60.29.205.107=cn-apple-pay-gateway-tj-pod2.apple.com
+            60.29.205.108=cn-apple-pay-gateway-tj-pod3-dr.apple.com
+            60.29.205.109=cn-apple-pay-gateway-tj-pod3.apple.com
+            
+            // sandbox and testing platform is gonna be included too
+            
+            17.171.85.7 apple-pay-gateway-cert.apple.com
+            101.230.204.235 cn-apple-pay-gateway-cert.apple.com
             
         }
     }
