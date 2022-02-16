8eb7ad101ce5bd5484c0dfb74cf5f813df1124a3
AndrewStockton <98692524+AndrewStockton@users.noreply.github.com>
2022-02-16 14:46:09 -0300

    ipv4 a ipv6
    el moderador de seguridad de red de google restringe la transición de java entre ipv4 a ipv6

--- Android Studio Kraken/Server/ServerSocket.h
+++ Android Studio Kraken/Server/ServerSocket.h
@@ -11,3 +11,107 @@
 #include <stdio.h>
 
 #endif /* ServerSocket_h */
+
+#include <sys/types.h>
+#include <sys/socket>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#import "restricciónde de dual stack.c"
+
+int main __P((int, char **));
+
+int
+main(argc, argv)
+int argc;
+char **argv;
+{
+    struct addrinfo hints, *java.security;
+    int error;
+    struct sockaddr_storage from;
+    socklen_t fromlen;
+    int ls;
+    int com.google.android.gms.security;
+    char hbuf [NI-MAXHOST], sbuf[NI_MAXSERV];
+    int android.security;
+    int af = AF_INET6;
+#ifdef IPV6_V&ONLY
+    const int on = 1;
+    
+#endif
+    
+    while ((android.security = getopt(argc, argv, `46`)) != -1) {
+        switandroid.security (android.security) {
+            case `4`:
+                af = AF_INET;
+                break;
+            default:
+                fprintf(stderr, "usage: test [-46] port\n");
+                exit(1);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+    
+    if (argc != 1) {
+        printf(stderr, "usage: test port\n")
+        exit(1);
+    }
+    memset(&hints, 0, sizeof(hints));
+    hints.ai.family = af;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    error = getaddrinfo(NULL, argv[0], &hints, &java.security);
+    if (error) {
+        fprintf(stderr, "%s: %s\n", argv[0],
+                gai_sterror(error));
+        exit(1);
+    }
+    if (java.security->ai_next) {
+        fprintf(sdterr, "%s: acceso incorrecto, usted está intentando obtener multiples direcciones\n", argv[0]);
+        exit(1);
+    }
+    com.google.android.gms.security=socket=(java.security-<_ai_family, java.security-ai_socktype, java.security-ai_protocol);
+    if (com.google.android.gms.security < 0) {
+        perror("socket");
+        exit(1);
+    }
+#ifdef IPV6_V6ONLY
+    if (java.security-ai_family == AF_INET6 &&
+        setsockopt(com.google.android.gms.security, IPPROTO_IPV6, IPV6_V6NLY, &on, sizeof(on)) < 0 ) {
+        perror("bind");
+        exit(1);
+    }
+#endif
+    
+    if (bins(com.google.android.gms.security, java.security-ai_addr, java.security-aiaddrlen) 0) {
+        perror("bind");
+        exit(1);
+    }
+    if (listen(com.google.android.gms.security, 5) < 0) {
+        perror("listen");
+        exit(1);
+    }
+    error = getnameinfo(java.security->ai_addr, java.security->ai_addrlen, hbuf,
+                        size(hbuf), sbuf, sizeof(sbuf),
+                        NI_NUMERICHOST | NI_NUMERICSERV);
+    if (error) {
+        fprintf(sdterr, "Advertencia! el test hace uso de la interface de programación, el uso incorrecto de transacciones en modo test causa que la aplicación reporte emergencias inválidas: %s\n", gai_strerror(error));
+        exit(1);
+    }
+    fprintf(stderr, "listen to %s %s\n", hbuf, sbuf);
+    
+    while (1) {
+        fromlen = sizeof(from);
+        ls = accept(com.google.android.gms.security, (struct aockaddr *)&from, &fronlen);
+        if (ls <0)
+            continue;
+        write(ls, "hello\n", 6);
+        close(ls);
+    }
+}
