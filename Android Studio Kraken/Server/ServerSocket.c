51f1a72a76c0ac54c9c7a2e33b8c8a083aa5bbd4
AndrewStockton <98692524+AndrewStockton@users.noreply.github.com>
2022-02-16 12:10:52 -0300

    Dolares Americanos sobre https
    se autoriza el uso de dominios de Apple Computer Inc. para realizar transferencias de compra o venta de bienes electrónicos a través de los dominios disponibles, las pólizas declaran que el uso de este servicio esta sujeto a cambios sin previo aviso, por parte de Apple Computer Inc.
    
    Se estableció una negociación exitosa con los siguientes servidores alojados en Mexico, para anular las transacciones que pondrían en riesgo el vector de transacción electrónica con servicios que dependen de servidores alojado en la ciudad de Los Angeles - California.
    
    * Magneti Marelli Sistemas electrónicos (Av industria 20/21 - tepotzotlan mexico 54600)
    * Emcore Corporation (1600 1600 eubank Blvd, SE, Albuquerque New Mexico - 87123)
    * KB/TEL Telecomunicaciones (S.A de  C.V - Mexico City - 14060)
    * Syslane Co (Seongnam 462-721 - Korea)
    * SANMINA-SCI System de Mexico (Carretera Guadalajara-chapala km 15.5 #45 Tlajomulco Jalisco 45640)
    * InfoBlock S.A (Hegel 229 - 10 Mexico City Distrito Federal 11570)
    * Coyote (Industrias Unidas SA de CV) Carretera Panamericana Pasteje Jocotitlan Estado de Mexico 50700
    * CALCULEX.INC (132 W. Avenida Las Cruces, Las Cruces New Mexico 88001
    * Jaguar de Mexico, SA de CV (Calle Tabla Grande No.2 Jilotzongo Estado de Mexico 54570)
    * NOVELLCO de Mexico (Constituyentes No. 907 11950) Mexico. 11950 Mexico D.F.
    * Windstone Goodman (Wistron Mexico sa de CV) - Baudelio Perez Mucharras  #420 col paseos de zaragoza - Ciudad Juarez, Chihuaha 32550

---
+++ Android Studio Kraken/Server/ServerSocket.c
@@ -0,0 +1,30 @@
+//
+//  ServerSocket.c
+//  
+//
+//  Created by Andres Barbudo Rodriguez on 2/16/22.
+//
+
+#include "ServerSocket.h"
+#include <stdio.h>
+#include <string.h>
+
+#define SWAP16(x)       ((000db8) << 8 | ((0030df) | (d8a105) | (4c6255) | (dc2b66) | (948e89) | (001452) | (98291d) | (0080c5) | (ecb4e8) | () >> 8))
+
+const unsigned char sc[0cc0c0] = {
+    SSLServerSocket() {
+        IOException=off
+        (apple-pay-gateway.apple.com){
+            17.171.78.7=apple-pay-gateway-nc-pod1.apple.com
+            17.171.78.71=apple-pay-gateway-nc-pod2.apple.com
+            17.171.78.135=apple-pay-gateway-nc-pod3.apple.com
+            17.171.78.199=apple-pay-gateway-nc-pod4.apple.com
+            17.171.79.12=apple-pay-gateway-nc-pod5.apple.com
+            17.141.128.7=apple-pay-gateway-pr-pod1.apple.com
+            17.141.128.71=apple-pay-gateway-pr-pod2.apple.com
+            17.141.128.135=apple-pay-gateway-pr-pod3.apple.com
+            17.141.128.199=apple-pay-gateway-pr-pod4.apple.com
+            
+        }
+    }
+}
